6e9743ca75820c06e131b3b3e91f4cc6e40c6887
Peter Nash <peter.nash@ichec.ie>
2011-07-08 15:06:13 +0100

    DL_POLY_4.02:CUDA Makefile

--- source/CUDA/link_cell_pairs_cuda.f90
+++ source/CUDA/link_cell_pairs_cuda.f90
@@ -1102,17 +1102,17 @@ Call start_timing_link_cell_pairs()
 
   End If
 
-	  Do i=1,natms
-      ii=ltg(i)
-      kk=list(0,i)
-      Do ll=1,kk
-         j=list(ll,i)
-         jj=ltg(j)
-          If (j <= natms .or. ii < jj) Then
-             Write(idnode+101,'(2i8)') ii,jj
-          End If
-    End Do
-  End Do
+!	  Do i=1,natms
+!      ii=ltg(i)
+!      kk=list(0,i)
+!      Do ll=1,kk
+!         j=list(ll,i)
+!         jj=ltg(j)
+!          If (j <= natms .or. ii < jj) Then
+!             Write(idnode+101,'(2i8)') ii,jj
+!          End If
+!    End Do
+!  End Do
 
   Deallocate (nix,niy,niz,                   Stat=fail(1))
   Deallocate (which_cell,at_list,            Stat=fail(2))
