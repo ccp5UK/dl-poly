4a7dff6555eed1b7e3333b721fdcc9e3227fd20f
Michael Seaton <michael.seaton@stfc.ac.uk>
2012-11-14 12:45:08 +0000

    DL_POLY_4.04.06: minor bug fix to metal_forces

--- source/metal_forces.f90
+++ source/metal_forces.f90
@@ -320,8 +320,8 @@ Subroutine metal_forces &
 
 ! interpolation parameters
 
-              If (rsq <= vmet(3,k0,1)**2) .or. & ! Next covers the FST density!
-                  (keypot /= 0 .and. rsq <= dmet(3,k0,1)**2) Then
+              If (rsq <= vmet(3,k0,1)**2 .or. & ! Next covers the FST density!
+                  (keypot /= 0 .and. rsq <= dmet(3,k0,1)**2)) Then
 
                  rdr = 1.0_wp/vmet(4,k0,1)
                  rrr = Sqrt(rsq) - vmet(2,k0,1)
