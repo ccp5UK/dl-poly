2362e353b44efc57bd2d2fcb6d2493d5b4cc26f6
Alin Marin Elena <alin@elena.space>
2015-10-23 09:38:03 +0100

    add modulefile template

---
+++ modulefile.cmake
@@ -0,0 +1,9 @@
+#%Module1.0
+
+proc ModulesHelp { } {
+  puts stderr "Simple module for DL_POLY @DLPOLY_VERSION@"
+}
+module-whatis   "Simple module for DL_POLY @DLPOLY_VERSION@"
+
+prepend-path PATH @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_BINDIR@
+
