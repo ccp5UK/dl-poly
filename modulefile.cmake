8cbeccf27eabc89b196fad5f9a9ee0e6f573e16d
Alin Marin Elena <alin-marin.elena@stfc.ac.uk>
2016-02-08 11:44:38 +0000

    [plumed] clean the mess I done with plumed commit

--- modulefile.cmake
+++
@@ -1,9 +0,0 @@
-#%Module1.0
-
-proc ModulesHelp { } {
-  puts stderr "Simple module for DL_POLY @DLPOLY_VERSION@"
-}
-module-whatis   "Simple module for DL_POLY @DLPOLY_VERSION@"
-
-prepend-path PATH @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_BINDIR@
-
