1819b7628df96be8e54abffa971dcf752adebfa7
Alin Marin Elena <alin-marin.elena@stfc.ac.uk>
2016-02-08 10:23:23 +0000

    update deployment scripts for plumed

--- build/build-dl-poly-plumed.sh
+++ build/build-dl-poly-plumed.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash 
 
 COMPILER=intel
+COMPILER=gnu
 if [ $COMPILER == "gnu" ]; then
-  module load openmpi/gcc/1.10.0 plumed/gnu/2.2.0
+  module load openmpi/gcc/1.10.1 plumed/gnu/2.2.1
   export FC=gfortran 
   export FFLAGS="-cpp -O3"
   #export FFLAGS="-cpp -g -O0 -std=f2008 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483648 -finit-real=nan -finit-logical=true -finit-character=42"  cmake $SOURCE_DIR  -DWITH_MPI=y -DWITH_PLUMED=y
@@ -13,8 +14,8 @@ else
 fi
 VERSION=4.09β
 DLPOLY_MODULE=/opt/modules/dlpoly/$COMPILER/$VERSION
-BUILD_DIR=/home/alin/playground/dl-poly-stfc-plumed/build/build-${COMPILER}-mpi-plumed-host
-SOURCE_DIR=/home/alin/playground/dl-poly-stfc-plumed
+BUILD_DIR=/home/alin/playground/dl-poly-alin-plumed/build/build-${COMPILER}-mpi-plumed-host
+SOURCE_DIR=/home/alin/playground/dl-poly-alin-plumed
 
 [[ -d $BUILD_DIR ]] && rm -rf $BUILD_DIR 
 mkdir -p $BUILD_DIR
--- build/build-plumed.sh
+++ build/build-plumed.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-
+set -x
 #module list
-COMPILER=intel
-#COMPILER=gnu
+#COMPILER=intel
+COMPILER=gnu
 libsuffix=64
 if [ $COMPILER == "gnu" ]; then
+  module load openmpi/gcc
   export CC=gcc
   export CXX=mpic++
   export FC=gfortran
@@ -18,23 +19,26 @@ else
   export CXXFLAGS="-O3 -xHost -g" 
   export CFLAGS="-O3 -xHost -g"
 fi
-PLUMED_VERSION=2.2.0
+PLUMED_VERSION=2.2.1
 BASE=/opt
-PLUMED_FOLDER=plumed2-${PLUMED_VERSION}
-PLUMED_SRC=${PLUMED_FOLDER}.tar.gz
+PLUMED_FOLDER=plumed-${PLUMED_VERSION}
+PLUMED_SRC=${PLUMED_FOLDER}.tgz
 BUILD_FOLDER=build-$COMPILER
 PLUMED_INSTALL=$BASE/plumed/$COMPILER/$PLUMED_VERSION
 PLUMED_MODULE=$BASE/modules/plumed/$COMPILER/$PLUMED_VERSION
-VMD_PLUGIN=/usr/lib64/vmd/1.9.2beta1.1427136773/plugins
-VMD_INC="-I$VMD_PLUGIN/include -I$VMD_PLUGIN/LINUXAMD64/molfile "
+#VMD_PLUGIN=/usr/lib64/vmd/1.9.2beta1.1427136773/plugins
+VMD_PLUGIN=
+#VMD_INC="-I$VMD_PLUGIN/include -I$VMD_PLUGIN/LINUXAMD64/molfile "
+VMD_INC=
 LIBS="-lnetcdf -ltcl8.6 "
+LDLF="-L$VMD_PLUGIN/LINUXAMD64/molfile"
 rm -rf $BUILD_FOLDER
 mkdir -p $BUILD_FOLDER
 pushd $BUILD_FOLDER
 tar -xf ../$PLUMED_SRC
 pushd ${PLUMED_FOLDER}
-patch -p1 < ../../0001-fix-template-issue-for-optimalAlignment.patch
-./configure LDFLAGS="-L$VMD_PLUGIN/LINUXAMD64/molfile" CPPFLAGS="$VMD_INC" LIBS="$LIBS"  CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS" \
+#patch -p1 < ../../0001-fix-template-issue-for-optimalAlignment.patch
+./configure LDFLAGS="$LDFL" CPPFLAGS="$VMD_INC" LIBS="$LIBS"  CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS" \
  --prefix=$PLUMED_INSTALL \
   --enable-external-molfile-plugins --enable-external-lapack \
  --enable-mpi --enable-openmp --enable-modules=crystallization
