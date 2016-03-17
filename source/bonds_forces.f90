65d33a70b3a3d0547aaafd8284b5bf5ac60686a3
Ilian Todorov <ilian.todorov@stfc.ac.uk>
2016-03-17 15:28:14 +0000

    bonds_forces and manual fixes

--- source/bonds_forces.f90
+++ source/bonds_forces.f90
@@ -343,10 +343,8 @@ Subroutine bonds_forces(isw,engbnd,virbnd,stress,rcut,keyfce,alpha,epsq,engcpe,v
 
            e0=2.55_wp*dr
 
-           term=k*dr*(1.0_wp-e0*(1.0_wp-(7.0_wp/12.0_wp)*e0))
-
-           omega=term*dr
-           gamma=(4.0_wp*term+k*dr*(-2.0_wp+e0))/rab
+           omega=k*dr**2*(1.0_wp-e0*(1.0_wp-(7.0_wp/12.0_wp)*e0))
+           gamma=2.0_wp*k*dr*(1.0_wp-e0*dr*(1.5_wp+2.0_wp*(7.0_wp/12.0_wp)*e0))/rab
 
         Else If (keyb == 20) Then
 
