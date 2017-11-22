290d44dec31dc68ff0b385800b57e8128681e2e2
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2017-11-22 08:46:06 +0000

    add a target for ford when build with documentation on -DDOCS=on

---
+++ cmake/dlpoly.md.cmake
@@ -0,0 +1,20 @@
+project: DL_POLY_4
+version: 4.09
+author: Ilian Todorov; Bill Smith
+date: November, 2017 
+year: 1992-2017
+src_dir: @CMAKE_SOURCE_DIR@/source
+output_dir: @CMAKE_BINARY_DIR@/docs
+graph:true
+graph_dir: @CMAKE_BINARY_DIR@/docs/graphs
+source:true
+search: false
+coloured_edges:true
+exclude_dir: ../source/CUDA
+exclude: ../source/SERIAL/ewald_spme_mfie~d_d.f90
+project_url: file://@CMAKE_BINARY_DIR@/docs
+license: stfc
+print_creation_date: true
+
+
+
