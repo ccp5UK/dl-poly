8f43f761ab478e85f6eecc6768106f1b24db3962
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2017-12-07 13:20:08 +0000

    add traceback for intel testing try to get more info about Issue #49

--- utils/intel-testing.sh
+++ utils/intel-testing.sh
@@ -4,7 +4,7 @@ module load intel plumed/intel
 mpr=`which mpirun` 
 mkdir build-intel-testing
 pushd build-intel-testing
-FC=ifort FFLAGS="-O1 " cmake ../ -DBUILD_TESTING=ON  -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DBUILDER="Gitlab Slave" -DWITH_PLUMED=on
+FC=ifort FFLAGS="-O3 -g -traceback" cmake ../ -DBUILD_TESTING=ON  -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DBUILDER="Gitlab Slave" -DWITH_PLUMED=on
 make -j10
 make test
 
