71a18838bbf3ac8cdb8d782e72c3f1a2e5e22f4a
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2018-03-20 18:11:42 +0000

    modularise msd and mpoles_rotmat

--- source/deport_data.F90
+++ source/deport_data.F90
@@ -2,7 +2,7 @@ Module deport_atomic
 
   Use kinds,            Only : wp
   Use comms,            Only : comms_type,gcheck,wp_mpi, Deport_tag, &
-                               Export_tag, MetLdExp_tag
+                               Export_tag, MetLdExp_tag, ExpMplRM_tag
 
   Use setup_module
   Use domains_module
@@ -33,7 +33,8 @@ Module deport_atomic
 
   Use ewald,               Only : ewald_type
   Use mpole ,              Only : keyind,ltpatm,lchatm, &
-                                 induce,indipx,indipy,indipz,rsdx,rsdy,rsdz
+                                 induce,indipx,indipy,indipz,rsdx,rsdy,rsdz, &
+                                 mplgfr,mprotx,mproty,mprotz, mplflg
 
   Use msd_module
   Use greenkubo_module,    Only : vxi,vyi,vzi,vafsamp
@@ -2486,6 +2487,304 @@ Subroutine metal_ld_set_halo(comm)
   End If
 
 End Subroutine metal_ld_set_halo
+
+
+Subroutine mpoles_rotmat_export(mdir,mlast,ixyz0,comm)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! dl_poly_4 routine to export multipoles rotation and infinitesimally
+! rotated matrices in the halo
+!
+! copyright - daresbury laboratory
+! author    - i.t.todorov march 2016
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  Integer, Intent( In    ) :: mdir
+  Integer, Intent( InOut ) :: mlast,ixyz0(1:mxatms)
+  Type( comms_type ), Intent( InOut ) :: comm
+
+
+  Logical           :: safe
+  Integer           :: fail,iadd,limit,iblock,          &
+                       i,j,jxyz,kxyz,ix,iy,iz,kx,ky,kz, &
+                       idl1,idl2,idl3,idl4,             &
+                       jdnode,kdnode,imove,jmove,itmp
+
+  Real( Kind = wp ), Dimension( : ), Allocatable :: buffer
+
+! Number of transported quantities per particle
+
+  iadd=4*mximpl+1
+  idl1=mximpl ; idl2=2*mximpl ; idl3=3*mximpl ; idl4=4*mximpl
+
+  fail=0 ; limit=iadd*mxbfxp ! limit=Merge(1,2,mxnode > 1)*iblock*iadd
+  Allocate (buffer(1:limit), Stat=fail)
+  If (fail > 0) Then
+     Write(nrite,'(/,1x,a,i0)') 'export_atomic_positions allocation failure, node: ', comm%idnode
+     Call error(0)
+  End If
+
+! Set buffer limit (half for outgoing data - half for incoming)
+
+  iblock=limit/Merge(2,1,comm%mxnode > 1)
+
+! DIRECTION SETTINGS INITIALISATION
+
+! define the neighbouring domains as sending and receiving with
+! respect to the direction (mdir)
+! k.   - direction selection factor
+! jxyz - halo reduction factor
+! ls.  - wrap-around +1 in . direction (domain on the left MD cell border)
+! le.  - wrap-around -1 in . direction (domain on the right MD cell border)
+! jdnode - destination (send to), kdnode - source (receive from)
+
+  kx = 0 ; ky = 0 ; kz = 0
+  If      (mdir == -1) Then ! Direction -x
+     kx  = 1
+     jxyz= 1
+     kxyz= 3
+
+     jdnode = map(1)
+     kdnode = map(2)
+  Else If (mdir ==  1) Then ! Direction +x
+     kx  = 1
+     jxyz= 2
+     kxyz= 3
+
+     jdnode = map(2)
+     kdnode = map(1)
+  Else If (mdir == -2) Then ! Direction -y
+     ky  = 1
+     jxyz= 10
+     kxyz= 30
+
+     jdnode = map(3)
+     kdnode = map(4)
+  Else If (mdir ==  2) Then ! Direction +y
+     ky  = 1
+     jxyz= 20
+     kxyz= 30
+
+     jdnode = map(4)
+     kdnode = map(3)
+  Else If (mdir == -3) Then ! Direction -z
+     kz  = 1
+     jxyz= 100
+     kxyz= 300
+
+     jdnode = map(5)
+     kdnode = map(6)
+  Else If (mdir ==  3) Then ! Direction +z
+     kz  = 1
+     jxyz= 200
+     kxyz= 300
+
+     jdnode = map(6)
+     kdnode = map(5)
+  Else
+     Call error(176)
+  End If
+
+! Initialise counters for length of sending and receiving buffers
+! imove and jmove are the actual number of particles to get haloed
+
+  imove=0
+  jmove=0
+
+! Initialise array overflow flags
+
+  safe=.true.
+
+! LOOP OVER ALL PARTICLES ON THIS NODE
+
+  Do i=1,mlast
+
+! If the particle is within the remaining 'inverted halo' of this domain
+
+     If (ixyz0(i) > 0) Then
+
+! Get the necessary halo indices
+
+        ix=Mod(ixyz0(i),10)           ! [0,1,2,3=1+2]
+        iy=Mod(ixyz0(i)-ix,100)       ! [0,10,20,30=10+20]
+        iz=Mod(ixyz0(i)-(ix+iy),1000) ! [0,100,200,300=100+200]
+
+! Filter the halo index for the selected direction
+
+        j=ix*kx+iy*ky+iz*kz
+
+! If the particle is within the correct halo for the selected direction
+
+        If (j == jxyz .or. (j > jxyz .and. Mod(j,3) == 0)) Then
+
+! If safe to proceed
+
+           If ((imove+iadd) <= iblock) Then
+
+! pack rotation matrices and infinitely rotated matrices
+
+              buffer(imove+1        : imove + idl1) = mplgfr(:,i)
+              buffer(imove+1 + idl1 : imove + idl2) = mprotx(:,i)
+              buffer(imove+1 + idl2 : imove + idl3) = mproty(:,i)
+              buffer(imove+1 + idl3 : imove + idl4) = mprotz(:,i)
+
+! Use the corrected halo reduction factor when the particle is halo to both +&- sides
+
+              buffer(imove+iadd)=Real(ixyz0(i)-Merge(jxyz,kxyz,j == jxyz),wp)
+
+           Else
+
+              safe=.false.
+
+           End If
+
+           imove=imove+iadd
+
+        End If
+
+     End If
+
+  End Do
+
+! Check for array bound overflow (have arrays coped with outgoing data)
+
+  Call gcheck(comm,safe)
+  If (.not.safe) Then
+     itmp=Merge(2,1,comm%mxnode > 1)*imove
+     Call gmax(comm,itmp)
+     Call warning(150,Real(itmp,wp),Real(limit,wp),0.0_wp)
+     Call error(178)
+  End If
+
+! exchange information on buffer sizes
+
+  If (comm%mxnode > 1) Then
+     Call MPI_IRECV(jmove,1,MPI_INTEGER,kdnode,ExpMplRM_tag,comm%comm,comm%request,comm%ierr)
+     Call MPI_SEND(imove,1,MPI_INTEGER,jdnode,ExpMplRM_tag,comm%comm,comm%ierr)
+     Call MPI_WAIT(comm%request,comm%status,comm%ierr)
+  Else
+     jmove=imove
+  End If
+
+! Check for array bound overflow (can arrays cope with incoming data)
+
+  safe=((mlast+jmove/iadd) <= mxatms)
+  Call gcheck(comm,safe)
+  If (.not.safe) Then
+     itmp=mlast+jmove/iadd
+     Call gmax(comm,itmp)
+     Call warning(160,Real(itmp,wp),Real(mxatms,wp),0.0_wp)
+     Call error(180)
+  End If
+
+! exchange buffers between nodes (this is a MUST)
+
+  If (comm%mxnode > 1) Then
+     If (jmove > 0) Call MPI_IRECV(buffer(iblock+1),jmove,wp_mpi,kdnode,ExpMplRM_tag,comm%comm,comm%request,comm%ierr)
+     If (imove > 0) Call MPI_SEND(buffer(1),imove,wp_mpi,jdnode,ExpMplRM_tag,comm%comm,comm%ierr)
+     If (jmove > 0) Call MPI_WAIT(comm%request,comm%status,comm%ierr)
+  End If
+
+! load transferred data
+
+  j=Merge(iblock,0,comm%mxnode > 1)
+  Do i=1,jmove/iadd
+     mlast=mlast+1
+
+! unpack rotation matrices and infinitesimal rotation matrices
+
+     mplgfr(:,mlast) = buffer(j+1        : j + idl1)
+     mprotx(:,mlast) = buffer(j+1 + idl1 : j + idl2)
+     mproty(:,mlast) = buffer(j+1 + idl2 : j + idl3)
+     mprotz(:,mlast) = buffer(j+1 + idl3 : j + idl4)
+
+     ixyz0(mlast)=Nint(buffer(j+iadd))
+     j=j+iadd
+  End Do
+
+  Deallocate (buffer, Stat=fail)
+  If (fail > 0) Then
+     Write(nrite,'(/,1x,a,i0)') 'mpoles_rotmat_export deallocation failure, node: ', comm%idnode
+     Call error(0)
+  End If
+
+End Subroutine mpoles_rotmat_export
+
+
+Subroutine mpoles_rotmat_set_halo(comm)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! dl_poly_4 routine to set and infinitesimally rotate native multipoles
+! rotation matrices and then arrange exchange of halo data between
+! neighbouring domains/nodes
+!
+! copyright - daresbury laboratory
+! author    - i.t.todorov february 2016
+! contrib   - h.a.boateng february 2016
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  Type( comms_type), Intent( InOut ) :: comm
+
+  Logical :: safe
+  Integer :: fail,i,mlast
+
+  Integer, Allocatable :: ixyz0(:)
+
+  Do i=1,natms
+     mplflg(i)=0
+     If (mxompl < 3) Then
+        Call rotate_mpoles_d(i)
+     Else
+        Call rotate_mpoles(i)
+     End If
+  End Do
+
+! Communicate the matrices in the halo
+
+  fail = 0
+  Allocate (ixyz0(1:mxatms), Stat = fail)
+  If (fail > 0) Then
+     Write(nrite,'(/,1x,a,i0)') 'mpoles_rotmat_set_halo allocation failure, node: ', comm%idnode
+     Call error(0)
+  End If
+  ixyz0(1:nlast) = ixyz(1:nlast)
+
+! No halo, start with domain only particles
+
+  mlast=natms
+
+! exchange atom data in -/+ x directions
+
+  Call mpoles_rotmat_export(-1,mlast,ixyz0,comm)
+  Call mpoles_rotmat_export( 1,mlast,ixyz0,comm)
+
+! exchange atom data in -/+ y directions
+
+  Call mpoles_rotmat_export(-2,mlast,ixyz0,comm)
+  Call mpoles_rotmat_export( 2,mlast,ixyz0,comm)
+
+! exchange atom data in -/+ z directions
+
+  Call mpoles_rotmat_export(-3,mlast,ixyz0,comm)
+  Call mpoles_rotmat_export( 3,mlast,ixyz0,comm)
+
+! check atom totals after data transfer
+
+  safe=(mlast == nlast)
+  Call gcheck(comm,safe)
+  If (.not.safe) Call error(174)
+
+  Deallocate (ixyz0, Stat = fail)
+  If (fail > 0) Then
+     Write(nrite,'(/,1x,a,i0)') 'mpoles_rotmat_set_halo deallocation failure, node: ', comm%idnode
+     Call error(0)
+  End If
+
+End Subroutine mpoles_rotmat_set_halo
 
 
 End Module deport_atomic
