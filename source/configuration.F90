da96e8e33402943946d78d81819ed83079124751
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2018-03-22 13:37:24 +0000

    remove scale_config add in configuration

--- source/configuration.F90
+++ source/configuration.F90
@@ -24,7 +24,7 @@ Module configuration
                              IO_READ_MASTER, IO_READ_NETCDF
 
   Use domains_module, Only : nprx,npry,nprz,nprx_r,npry_r,nprz_r,idx,idy,idz
-
+  Use development_module, Only : lvcfscl,cels
 #ifdef SERIAL
   Use mpi_api
 #else
@@ -2330,6 +2330,61 @@ Subroutine scan_config(megatm,imc_n,dvar,cfgname,levcfg,imcon,cell,xhi,yhi,zhi,c
   Call error(55)
 
 End Subroutine scan_config
+
+Subroutine scale_config(megatm)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! dl_poly_4 subroutine for rescaling the crystallographic information
+! from CONFIG to new lattice parameters and saving it in CFGSCL
+!
+! copyright - daresbury laboratory
+! author    - i.t.todorov february 2015
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  Integer, Intent( In    ) :: megatm
+
+  Character ( Len = 6 ) :: name
+  Integer               :: i,nstep
+  Real( Kind = wp )     :: rcell(1:9),det,uuu,vvv,www,tstep,time
+
+! Get the inverse cell matrix
+
+  Call invert(cell,rcell,det)
+
+! Rescale
+
+  Do i=1,natms
+     uuu=xxx(i)
+     vvv=yyy(i)
+     www=zzz(i)
+
+     xxx(i)=rcell(1)*uuu+rcell(4)*vvv+rcell(7)*www
+     yyy(i)=rcell(2)*uuu+rcell(5)*vvv+rcell(8)*www
+     zzz(i)=rcell(3)*uuu+rcell(6)*vvv+rcell(9)*www
+
+     uuu=xxx(i)
+     vvv=yyy(i)
+     www=zzz(i)
+
+     xxx(i)=cels(1)*uuu+cels(4)*vvv+cels(7)*www
+     yyy(i)=cels(2)*uuu+cels(5)*vvv+cels(8)*www
+     zzz(i)=cels(3)*uuu+cels(6)*vvv+cels(9)*www
+  End Do
+
+! Write REVCON
+
+  name   = 'CFGSCL' ! file name
+  nstep  = 0        ! no steps done
+  tstep  = 0.0_wp   ! no step exists
+  time   = 0.0_wp   ! time is not relevant
+
+  rcell = cell ; cell = cels
+  Call write_config(name,lvcfscl,megatm,nstep,tstep,time)
+  cell = rcell
+
+End Subroutine scale_config
 
 
 End Module configuration
