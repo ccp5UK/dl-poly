45f0e081599656183da8d135a89319530522b5a4
Ivan Scivetti <ivan.scivetti@stfc.ac.uk>
2018-04-04 15:05:37 +0100

    Fix error messages in nst_mtk.F90

--- source/nst_mtk.F90
+++ source/nst_mtk.F90
@@ -121,6 +121,8 @@ Contains
 
     Real( Kind = wp ), Allocatable, Save :: dens0(:)
 
+    Character ( Len = 256 )  ::  message
+
     fail=0
     If (megcon > 0 .or. megpmf > 0) Then
        Allocate (lstitr(1:mxatms),                                  Stat=fail(1))
@@ -138,8 +140,8 @@ Contains
     Allocate (vxt(1:mxatms),vyt(1:mxatms),vzt(1:mxatms),            Stat=fail(8))
     Allocate (fxt(1:mxatms),fyt(1:mxatms),fzt(1:mxatms),            Stat=fail(9))
     If (Any(fail > 0)) Then
-       Write(nrite,'(/,1x,a,i0)') 'nst_m0 allocation failure, node: ', comm%idnode
-       Call error(0)
+       Write(message,'(a)') 'nst_m0 allocation failure'
+       Call error(0,message)
     End If
 
 
@@ -154,8 +156,8 @@ Contains
 
        Allocate (dens0(1:mxatyp), Stat=fail(1))
        If (fail(1) > 0) Then
-          Write(nrite,'(/,1x,a,i0)') 'dens0 allocation failure, node: ', comm%idnode
-          Call error(0)
+          Write(message,'(a)') 'dens0 allocation failure'
+          Call error(0,message)
        End If
        Do i=1,ntpatm
           dens0(i) = dens(i)
@@ -659,8 +661,8 @@ Contains
     Deallocate (vxt,vyt,vzt,         Stat=fail(8))
     Deallocate (fxt,fyt,fzt,         Stat=fail(9))
     If (Any(fail > 0)) Then
-       Write(nrite,'(/,1x,a,i0)') 'nst_m0 deallocation failure, node: ', comm%idnode
-       Call error(0)
+       Write(message,'(a)') 'nst_m0 deallocation failure'
+       Call error(0,message)
     End If
 
   End Subroutine nst_m0_vv
@@ -804,8 +806,8 @@ Contains
     Allocate (rgdvxt(1:mxrgd),rgdvyt(1:mxrgd),rgdvzt(1:mxrgd),      Stat=fail(13))
     Allocate (rgdoxt(1:mxrgd),rgdoyt(1:mxrgd),rgdozt(1:mxrgd),      Stat=fail(14))
     If (Any(fail > 0)) Then
-       Write(nrite,'(/,1x,a,i0)') 'nst_m1 allocation failure, node: ', comm%idnode
-       Call error(0)
+       Write(message,'(a)') 'nst_m1 allocation failure'
+       Call error(0,message)
     End If
 
 
@@ -820,8 +822,8 @@ Contains
 
        Allocate (dens0(1:mxatyp), Stat=fail(1))
        If (fail(1) > 0) Then
-          Write(nrite,'(/,1x,a,i0)') 'dens0 allocation failure, node: ', comm%idnode
-          Call error(0)
+          Write(message,'(a)') 'dens0 allocation failure'
+          Call error(0,message)
        End If
        Do i=1,ntpatm
           dens0(i) = dens(i)
