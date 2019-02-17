9231392233498f78cbcf748b849a209ed4f5cd09
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2019-02-17 11:24:18 +0000

    [ci] update testing scripts

--- utils/intel-testing.sh
+++ utils/intel-testing.sh
@@ -5,8 +5,6 @@ mpr=`which mpirun`
 rm -rf build-intel-testing
 mkdir build-intel-testing
 pushd build-intel-testing
-FC=ifort FFLAGS="-O3 -g -traceback" cmake ../ -DBUILD_TESTING=ON  -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DBUILDER="Gitlab Slave" 
-make -j10
-ctest -j 1 -E TEST28
+FC=ifort FFLAGS="-O3 -g -traceback" cmake ../ -DBUILD_TESTING=ON  -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DBUILDER="Gitlab Slave"  && make -j10 && ctest -j 1 -E TEST28
 
 
