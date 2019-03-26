dea2d1759f2326fc02e02ae0550e855780ae2dd4
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2019-03-26 08:22:57 +0000

    update debug documentations and testing follow up from #401

--- utils/mpi-testing-paranoid.sh
+++ utils/mpi-testing-paranoid.sh
@@ -5,6 +5,6 @@ module load gnu openmpi/3.1.1
 
 folder="build-mpi-testing-paranoid" 
 rm -rf $folder && mkdir $folder && pushd $folder
-FFLAGS="-g -frecord-gcc-switches -O0 -std=f2008 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42 -finit-derived -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe" cmake ../ -DBUILD_TESTING=ON -DWITH_PLUMED=Off -DBUILDER="Gitlab Worker" && make -j10 && ctest --timeout 200
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DWITH_PLUMED=Off -DBUILDER="Gitlab Worker" && make -j10 && ctest --timeout 200
 
 
