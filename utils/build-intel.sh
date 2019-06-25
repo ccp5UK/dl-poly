f6284d3f88fb99b1fa361a9f96d7c1ed4bc2c81c
Ivan Scivetti <ivan.scivetti@stfc.ac.uk>
2019-06-26 00:35:33 +0100

    Adding build-intel.sh again4

--- utils/build-intel.sh
+++ utils/build-intel.sh
@@ -4,6 +4,7 @@ module load intel
 module load gnu/7 openblas/0.3.5 
 mkdir -p build-mpi-intel
 pushd build-mpi-intel
-FC=ifort cmake ../ -DMPI_Fortran_COMPILER=mpiifort -DWITH_EVB=ON
+FC=ifort cmake ../ -DMPI_Fortran_COMPILER=mpiifort 
+#-DWITH_EVB=ON
 make -j10
 
