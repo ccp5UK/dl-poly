faf40dca518ae61ea0ffc0e9e34611845799e85c
Ivan Scivetti <ivan.scivetti@stfc.ac.uk>
2019-06-26 00:18:13 +0100

    Adding build-intel.sh again2

--- utils/build-intel.sh
+++ utils/build-intel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-module load intel openblas
+module load intel openblas/0.3.5 
 mkdir -p build-mpi-intel
 pushd build-mpi-intel
 FC=ifort cmake ../ -DMPI_Fortran_COMPILER=mpiifort -DWITH_EVB=ON
