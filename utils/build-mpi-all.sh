8d730cfd2d619eff75c806c1b4dadce5fec11482
Ivan Scivetti <ivan.scivetti@stfc.ac.uk>
2019-06-26 00:53:20 +0100

    Adding build-mpi-all.sh again

--- utils/build-mpi-all.sh
+++ utils/build-mpi-all.sh
@@ -2,7 +2,7 @@
 module load gnu openmpi/3.1.1 netcdf-fortran/4.4.4  pnetcdf/4.6.1
 module load plumed/gnu
 module load kim/gcc/2.0.0
-module load openblas/0.3.5
+module load openblas
 mkdir build-mpi-all
 pushd build-mpi-all
 cmake ../ -DWITH_KIM=ON -DWITH_PLUMED=ON -DWITH_NETCDF=ON -DWITH_EVB=ON
