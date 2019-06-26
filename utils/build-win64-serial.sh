ffedbc30967164014dd6966e36113e1a0ac7203c
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2019-06-26 06:57:59 +0100

    Update build-win64-serial.sh

--- utils/build-win64-serial.sh
+++ utils/build-win64-serial.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build-win64-serial
 pushd build-win64-serial
-FFLAGS="-O3 -static" cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/win64helper.cmake -DWITH_MPI=OFF -DWITH_EVB=ON
+FFLAGS="-O3 -static" cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/win64helper.cmake -DWITH_MPI=OFF -DWITH_EVB=OFF
 make -j10
