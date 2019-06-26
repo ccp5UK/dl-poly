7d37cb193e6311f5ed2565487e1f1e0556f8a9e5
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2019-06-26 06:58:54 +0100

    Update build-win32-serial.sh

--- utils/build-win32-serial.sh
+++ utils/build-win32-serial.sh
@@ -3,5 +3,5 @@
 module load openblas
 mkdir -p build-win32-serial
 pushd build-win32-serial
-FFLAGS="-O3 -static" cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/win32helper.cmake -DWITH_MPI=OFF -DWITH_EVB=ON
+FFLAGS="-O3 -static" cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/win32helper.cmake -DWITH_MPI=OFF -DWITH_EVB=Off
 make -j10
