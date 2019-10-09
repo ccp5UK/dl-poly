6d5dd368033e4caa04971975401fa98358e18bf0
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2019-10-09 11:52:07 +0100

    add external dependencies to be build automatically, plumed and kim fixes #458

--- utils/build-mpi-kim-internal.sh
+++ utils/build-mpi-kim-internal.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 module load gnu openmpi/3.1.1
-module load kim/gcc/2.1.3
 
+rm -rf build-mpi-kim
 mkdir build-mpi-kim
 pushd build-mpi-kim
-cmake ../ -DWITH_KIM=ON -DINTERNAL_KIM=Off
+rm -rf $HOME/101
+cmake ../ -DWITH_KIM=ON -DCMAKE_INSTALL_PREFIX=$HOME/101
 make -j10
 
 
--- utils/mpi-testing.sh
+++ utils/mpi-testing.sh
@@ -5,6 +5,6 @@ module load plumed/gnu
 
 folder="build-mpi-testing" 
 rm -rf $folder && mkdir $folder && pushd $folder
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -DWITH_PLUMED=ON -DBUILDER="Gitlab Slave" && make -j10 && ctest -j 2 
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -DWITH_PLUMED=ON -DINTERNAL_PLUMED=off -DBUILDER="Gitlab Slave" && make -j10 && ctest -j 2 
 
 
