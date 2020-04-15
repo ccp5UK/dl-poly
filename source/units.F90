f4a82fc3ad63b9125d437c2892d6b43a58246089
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2020-04-15 13:04:02 +0100

    Fixed some more unit bugs

--- source/units.F90
+++ source/units.F90
@@ -34,61 +34,67 @@ contains
     !!-----------------------------------------------------------------------
     Real(kind=wp), Parameter :: planck_internal = 6.350780668_wp
     Real(kind=wp), Parameter :: electron_charge = 1.6021766208e-19_wp ! C
+    Real(kind=wp), Parameter :: coulomb = electron_charge
     Real(kind=wp), Parameter :: avogadro = 6.022140857e23_wp
 
     Real(kind=wp), Parameter :: metre = 1e-10_wp
-    Real(kind=wp), Parameter :: bohr = 0.52916_wp
+    Real(kind=wp), Parameter :: bohr = 1.8897875878751229_wp
 
     Real(kind=wp), Parameter :: joulepmol = 10.0_wp
     Real(kind=wp), Parameter :: caloriepmol = 1.0_wp / (4.1842_wp/joulepmol)
-    Real(kind=wp), Parameter :: ev = 0.00010364272224984791_wp
-    Real(kind=wp), Parameter :: hartree = ev*27.211396641308_wp
 
-    Real(kind=wp), Parameter :: kilogram = 6.022e26_wp
+    Real(kind=wp), Parameter :: hartree = 354.5768322068828 !(/ (* 0.00010364272224984791 27.211396641308))
+
+    Real(kind=wp), Parameter :: kilogram = 1.660577881102624e-27_wp
+    Real(kind=wp), Parameter :: pound = 3.6608612486140225e-27_wp
 
     Real(kind=wp), Parameter :: second = 1e-12_wp
 
     Real(kind=wp), Parameter :: atmosphere = 163.882576
-    Real(kind=wp), Parameter :: pascal = 6.02213665167516e-8_wp
+    Real(kind=wp), Parameter :: pascal = 16605402.0_wp
 
     Real(kind=wp), Parameter :: newton = kilogram*metre/second**2
 
-    Real(kind=wp), Parameter :: gravity = 9.81e-14_wp
+    Real(kind=wp), Parameter :: gravity = metre/second**2/9.81
 
     call units_table%init(100)
 
     ! Time
 
     call units_table%set("internal_t", init_unit(abbrev="internal_t", name="Picosecond", time=1, to_internal=1.0_wp))
-    call units_table%set("hr", init_unit(abbrev="hr", name="Hour", time=1, to_internal=3600.0_wp*second))
-    call units_table%set("min", init_unit(abbrev="min", name="Minute", time=1, to_internal=60.0_wp*second))
+    call units_table%set("hr", init_unit(abbrev="hr", name="Hour", time=1, to_internal=second/3600.0_wp))
+    call units_table%set("min", init_unit(abbrev="min", name="Minute", time=1, to_internal=second/60.0_wp))
     call units_table%set("s", init_unit(abbrev="s", name="Second", time=1, to_internal=second))
+    call units_table%set("aut", init_unit(abbrev="aut", name="Atomic Time Unit", time=1, to_internal=41341.373335182114_wp))
 
     ! Length
 
     call units_table%set("internal_l", init_unit(abbrev="internal_l", name="Angstrom", length=1, to_internal=1.0_wp))
-    call units_table%set("ang", init_unit(abbrev="ang", name="Angstrom", length=1, to_internal=1e-10_wp*metre))
+    call units_table%set("ang", init_unit(abbrev="ang", name="Angstrom", length=1, to_internal=1e10_wp*metre))
     call units_table%set("bohr", init_unit(abbrev="bohr", name="Bohr", length=1, to_internal=bohr))
     call units_table%set("m", init_unit(abbrev="m", name="Metre", length=1, to_internal=metre))
-    call units_table%set('"', init_unit(abbrev='"', name="Inch", length=1, to_internal=2.54e-2*metre))
+    call units_table%set('in', init_unit(abbrev="in", name="Inch", length=1, to_internal=metre/2.54e-2_wp))
+    call units_table%set("ft", init_unit(abbrev="ft", name="Foot", length=1, to_internal=metre/0.30479999999999996_wp))
 
     ! Mass
 
     call units_table%set("internal_m", init_unit(abbrev="internal_m", name="Atomic Mass Unit", mass=1, to_internal=1.0_wp))
     call units_table%set("da", init_unit(abbrev="Da", name="Atomic Mass Unit", mass=1, to_internal=1.0_wp))
     call units_table%set("amu", init_unit(abbrev="amu", name="Atomic Mass Unit", mass=1, to_internal=1.0_wp))
-    call units_table%set("g", init_unit(abbrev="g", name="Gram", mass=1, to_internal=1e-3*kilogram))
+    call units_table%set("g", init_unit(abbrev="g", name="Gram", mass=1, to_internal=1e3*kilogram))
+    call units_table%set("lb", init_unit(abbrev="lb", name="Pound", mass=1, to_internal=pound))
+    call units_table%set("oz", init_unit(abbrev="oz", name="Ounce", mass=1, to_internal=pound*16.0_wp))
 
     ! Charge
 
     call units_table%set("internal_q", &
          & init_unit(abbrev="internal_q", name="Elementary charge", current=1, time=1, to_internal=1.0_wp))
     call units_table%set("q_e", &
-         & init_unit(abbrev="q_e", name="Elementary charge", current=1, time=1, to_internal=electron_charge))
+         & init_unit(abbrev="q_e", name="Elementary charge", current=1, time=1, to_internal=1.0_wp))
     call units_table%set("e", &
-         & init_unit(abbrev="e", name="Elementary charge", current=1, time=1, to_internal=electron_charge))
+         & init_unit(abbrev="e", name="Elementary charge", current=1, time=1, to_internal=1.0_wp))
     call units_table%set("c", &
-         & init_unit(abbrev="C", name="Coulomb", current=1, time=1, to_internal=1.0_wp / electron_charge))
+         & init_unit(abbrev="C", name="Coulomb", current=1, time=1, to_internal=coulomb))
 
     ! Energy
 
@@ -98,7 +104,8 @@ contains
     call units_table%set("cal", &
          & init_unit(abbrev="Cal", name="Calorie", mass=1, length=2, time=-2, to_internal=caloriepmol*avogadro))
     call units_table%set("ha", init_unit(abbrev="Ha", name="Hartree", mass=1, length=2, time=-2, to_internal=hartree))
-    call units_table%set("ry", init_unit(abbrev="Ry", name="Rydberg", mass=1, length=2, time=-2, to_internal=0.5_wp*hartree))
+    call units_table%set("e_h", init_unit(abbrev="Ha", name="Hartree", mass=1, length=2, time=-2, to_internal=hartree))
+    call units_table%set("ry", init_unit(abbrev="Ry", name="Rydberg", mass=1, length=2, time=-2, to_internal=2.0_wp*hartree))
     call units_table%set("k", init_unit(abbrev="K", name="Kelvin", mass=1, length=2, time=-2, to_internal=boltz))
 
     ! Pressure
@@ -115,21 +122,28 @@ contains
     call units_table%set("n", &
          & init_unit(abbrev="N", name="Newton", mass=1, length=1, time=-2, to_internal=newton))
     call units_table%set("dyn", &
-         & init_unit(abbrev="dyn", name="Dyne", mass=1, length=1, time=-2, to_internal=1e-5_wp*newton))
+         & init_unit(abbrev="dyn", name="Dyne", mass=1, length=1, time=-2, to_internal=1e5_wp*newton))
+
+    ! Velocity
+
+    call units_table%set("internal_v", &
+         & init_unit(abbrev="ang/ps", name="Angstrom per picosecond", length=1, time=-1, to_internal=1.0_wp))
+    call units_table%set("auv", &
+         & init_unit(abbrev="aut", name="Atomic Velocity Unit", length=1, time=-1, to_internal=1.2799768302560889E-5_wp))
 
     ! Acceleration
 
     call units_table%set("grav", &
-         & init_unit(abbrev="grav", name="gravitation", length=1, time=-2, to_internal=gravity))
+         & init_unit(abbrev="g_e", name="9.81 m/s^2", length=1, time=-2, to_internal=gravity))
 
     ! Voltage
 
     call units_table%set("v", init_unit(abbrev="V", name="Volt", mass=1, length=2, time=-3, current=-1, &
-         & to_internal=1.0_wp/(electron_charge*second)))
+         & to_internal=joulepmol*avogadro/(coulomb)))
 
     ! Mols
 
-    call units_table%set("mol", init_unit(abbrev="mol", name="mole", mol=1, to_internal=avogadro))
+    call units_table%set("mol", init_unit(abbrev="mol", name="Mole", mol=1, to_internal=avogadro))
 
     ! Unitless
 
@@ -158,6 +172,7 @@ contains
     Character(len=*), Intent(inout) :: string
     Integer :: i
     Type(unit_data), intent(out) :: factor
+    Character(Len=256) :: tmp
     Character(len=*), Parameter :: prefix_symbol = "YZEPTGMk dcmunpfazy"
     Type(unit_data), Dimension(19), Parameter :: prefix = [ &
          & unit_data(name="Yotta", abbrev="Y", conversion_to_internal=1e-24_wp), &
@@ -181,7 +196,9 @@ contains
          & unit_data(name="Yocto", abbrev="y", conversion_to_internal=1e24_wp)]
 
     factor = null_unit
-    if (.not. units_table%in(string(2:))) then
+    tmp = string(2:)
+    call lower_case(tmp)
+    if (.not. units_table%in(tmp)) then
        i = index(prefix_symbol, string(2:2))
        if (i < 1 .or. .not. units_table%in(string(3:))) call error(0, "Unit not found "//string(2:))
        factor = prefix(i)
@@ -206,25 +223,27 @@ contains
     output = null_unit
 
     ! Handle powers first
-    do i = 1, size(parsed)
-       if (parsed(i)(1:1) == "^") then
-          if (verify(parsed(i)(2:), number) /= 0) call error(0, "Non numeric power issued")
-          read(parsed(i)(2:), "(i8.1)") n
+    do i = size(parsed), 1, -1
+       curr_parse = parsed(i)
+       if (curr_parse(1:1) == "^") then
+          if (verify(trim(curr_parse(2:)), number) /= 0) call error(0, "Non numeric power issued")
+          read(curr_parse(2:), "(i8.1)") n
           curr_parse = parsed(i-1)
           call handle_decimal_prefix(curr_parse, factor)
           call lower_case(curr_parse)
           call units_table%get(curr_parse(2:), tmp_unit)
           tmp_unit = (factor*tmp_unit) ** n
-          select case (parsed(i-1)(1:1))
+          select case (curr_parse(1:1))
           case (".")
              output = output * tmp_unit
           case ("/")
              output = output / tmp_unit
           case default
+             print*, curr_parse(1:1)
              call error(0, "Cannot parse unit string"//string)
           end select
-          parsed(i) = ""
-          parsed(i-1) = ""
+          parsed(i) = "."
+          parsed(i-1) = "."
        end if
     end do
 
@@ -233,6 +252,7 @@ contains
        call handle_decimal_prefix(curr_parse, factor)
        call lower_case(curr_parse)
        call units_table%get(curr_parse(2:), tmp_unit)
+
        select case (curr_parse(1:1))
        case (".")
           output = output * (factor * tmp_unit)
@@ -250,7 +270,7 @@ contains
   Subroutine decompose_unit_string(string, output)
     Character(Len=256), Dimension(:), Allocatable :: output
     Character(Len=*), Intent( In ) :: string
-    Character(Len=*), Parameter :: alphabet = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+    Character(Len=*), Parameter :: alphabet = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'"//'"'
     Character(Len=*), Parameter :: punc = "./^"
     Integer :: nParts
     Integer :: i, j, k
