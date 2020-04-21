c0b6966293a579205cb4e573ec3d3ca0a9fd336d
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2020-04-21 11:16:12 +0100

    refresh reference data

--- utils/Testing/generateTests.sh
+++ utils/Testing/generateTests.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-DLROOT=/home/alin/dl-poly-internal
-data=new-data
+DLP_ROOT=/home/drFaustroll/playground/dlpoly/dl-poly-devel
+data="${DLP_ROOT}/data"
 
 genTest(){
   test=$1
   iterations="$2"
-  name=$(grep "TEST $test " $DLROOT/data/README.txt | awk -F \- '{print $2}') 
-  echo "processing test $test - $name"
-  pushd $data/TEST${test} >> /dev/null
-  echo "    <testcase name=\"TEST${test}\">" >> $testf
-  echo "      <path archive=\"Yes\">TEST${test}</path>" >> $testf
-  echo "      <description>TEST ${test} - ${name}</description>" >> $testf
+  name=$(grep "${test/TEST/TEST } " $DLP_ROOT/data/README.txt | cut -d "-" -f 2-) 
+  echo "processing $test - $name"
+  [[ ! -d $test ]] && tar -xf $data/${test}.tar.xz
+  pushd ${test} >> /dev/null
+  echo "    <testcase name=\"${test}\">" >> $testf
+  echo "      <path archive=\"Yes\">${test}</path>" >> $testf
+  echo "      <description>${test} - ${name}</description>" >> $testf
 
   for iteration in $iterations; do
     for prop in $props; do
-      val=$($DLROOT/utils/Scripts/${prop}.py $iteration)
+      val=$($DLP_ROOT/utils/Scripts/${prop}.py $iteration)
       echo "        <${prop} iteration=\"${iteration}\">${val}</${prop}>" >> $testf
-    done 
+    done
   done
-  rm -rf TEST${test}
-  popd >> /dev/null
   echo "    </testcase>" >> $testf
+  popd >> /dev/null
+  rm -rf ${test}
 }
 
 props=$(grep "property name" properties.xml | sed 's;<property name=";;g' | sed 's;">;;g' | xargs )
-testf="$DLROOT/utils/Testing/all.xml"
+testf="$DLP_ROOT/utils/Testing/all.xml"
 
 
 echo "<beetest>" > $testf
@@ -38,35 +39,14 @@ echo "    <date>$(date)</date>" >> $testf
 echo "    <author>Alin Marin Elena</author>" >> $testf
 echo "    <outdir>TestsOut</outdir>" >> $testf
    
-genTest 01 "0 10 20" 
-genTest 02 "0 10 20" 
-genTest 03 "0 10 20" 
-genTest 04 "0 10 20" 
-genTest 05 "0 10 20" 
-genTest 06 "0 10 20" 
-genTest 07 "0 10 20" 
-genTest 08 "0 10 20" 
-genTest 09 "0 10 20" 
-genTest 10 "0 10 20" 
-genTest 11 "0 10 20" 
-genTest 12 "0 10 20" 
-genTest 13 "0 10 20" 
-genTest 14 "0 10 20" 
-genTest 15 "0 10 20" 
-genTest 16 "0 10 20" 
-genTest 17 "0 10 20" 
-genTest 18 "0 10 20" 
-genTest 19 "0 10 20" 
-genTest 20 "0 10 25" 
-genTest 21 "0 10 20" 
-genTest 22 "0 10 20" 
-genTest 23 "0 10 20" 
-genTest 24 "0 10 20" 
-genTest 25 "0 10 20" 
-#genTest 28 "0 10 20" 
+
+for i in $(cat tests.dat); do
+  genTest $i "0 10 20"
+done 
+
 echo "  </testsuite>" >> $testf
 echo "</beetest>" >> $testf
 
 sed "s;Scripts;@PYTHON_EXECUTABLE@ @CMAKE_SOURCE_DIR@/utils/Scripts;g"  $testf > ${testf}.cmake
 rm $testf
-cp ${testf}.cmake $DLROOT/cmake
+cp ${testf}.cmake $DLP_ROOT/cmake
