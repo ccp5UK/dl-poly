c3b23c88355bb1f40890c556d10e588dae9b0ea8
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2020-06-08 12:22:58 +0100

    Remove excess printing, but make tests more informative

--- utils/intel-testing-paranoid.sh
+++ utils/intel-testing-paranoid.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 module load intel/2018.u3
-mpr=`which mpirun` 
+mpr=`which mpirun`
 rm -rf build-intel-testing-p
 mkdir build-intel-testing-p
 pushd build-intel-testing-p
-FC=ifort cmake ../ -DBUILD_TESTING=ON  -DCMAKE_BUILD_TYPE=Debug -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DMPIEXEC_PREFLAGS="-check-mpi" -DBUILDER="Gitlab Slave" -DWITH_PLUMED=off && make -j10 && ctest -j 1 -E TEST2[89]
-
-
+FC=ifort cmake ../ -DBUILD_TESTING=ON  -DCMAKE_BUILD_TYPE=Debug -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DMPIEXEC_PREFLAGS="-check-mpi" -DBUILDER="Gitlab Slave" -DWITH_PLUMED=off && make -j10 && ctest --output-on-failure -j 1 -E TEST2[89]
--- utils/mpi-testing.sh
+++ utils/mpi-testing.sh
@@ -3,7 +3,6 @@
 module load gnu openmpi/3.1.1 netcdf-fortran/4.4.4  pnetcdf/4.6.1
 module load plumed/gnu
 
-folder="build-mpi-testing" 
+folder="build-mpi-testing"
 rm -rf $folder && mkdir $folder && pushd $folder
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -DWITH_PLUMED=ON -DINTERNAL_PLUMED=off -DBUILDER="Gitlab Slave" && make -j10 && ctest -j 2 -E TEST29
-
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -DWITH_PLUMED=ON -DINTERNAL_PLUMED=off -DBUILDER="Gitlab Slave" && make -j10 && ctest --output-on-failure -j 2 -E TEST29
