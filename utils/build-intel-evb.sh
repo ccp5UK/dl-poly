7cdc4185928350f77812c855b86afed960d00271
Alin Marin Elena <alin@elena.space>
2020-09-18 09:56:07 +0100

    activate new tests for evb build

--- utils/build-intel-evb.sh
+++ utils/build-intel-evb.sh
@@ -2,8 +2,8 @@
 
 module load intel/2020a
 module load gnu openblas
-mkdir -p build-mpi-intel
-pushd build-mpi-intel
+mkdir -p build-mpi-intel-evb
+pushd build-mpi-intel-evb
 FC=ifort cmake ../ -DMPI_Fortran_COMPILER=mpiifort -DWITH_EVB=ON
 make -j10
 
--- utils/build-mpi-evb.sh
+++ utils/build-mpi-evb.sh
@@ -3,8 +3,8 @@
 module load gnu/7 openmpi/3.1.6
 module load openblas/0.3.10
 
-mkdir build-mpi-pure
-pushd build-mpi-pure
+mkdir build-mpi-pure-evb
+pushd build-mpi-pure-evb
 cmake ../ -DWITH_EVB=ON
 make -j10
 
