c34b02cfe3d19f5be7511e6194fc9452badbc216
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2020-09-25 13:51:51 +0100

    Flush before closing

--- source/errors_warnings.F90
+++ source/errors_warnings.F90
@@ -2384,7 +2384,7 @@ Contains
     Logical :: has_name, is_open
 
     Inquire (i, opened=is_open, named=has_name, iostat=ierr)
-
+    flush(i)
     If (is_open .and. has_name .and. ierr == 0 .and. All(i /= [-1, ERROR_UNIT, INPUT_UNIT, OUTPUT_UNIT])) Then
       Close (i)
     End If
