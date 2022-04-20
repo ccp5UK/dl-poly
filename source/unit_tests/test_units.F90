2e4b9894e4c470be2b99a8ce61baba9ed0447e74
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2022-04-20 18:23:53 +0000

    Out units revamp

--- source/unit_tests/test_units.F90
+++ source/unit_tests/test_units.F90
@@ -2,7 +2,8 @@ Module test_units
 
   Use asserts, only : assert
   Use constants, only : tenunt, boltz
-  Use units, only : initialise_units, convert_units, destroy_units
+  Use units, only : initialise_units, convert_units, destroy_units, &
+       & to_out_units, units_scheme, set_out_units, units_scheme, init_scheme
   Use kinds, only : wp
   Implicit None
 
@@ -17,6 +18,9 @@ contains
     Real( kind = wp ), Parameter :: atm_GPa = 0.00010132501_wp
     Real( kind = wp ), Parameter :: kB_JK = 1.3804703422756560E-023
     Real( kind = wp ) :: unit_test
+    Character(Len=256) :: unit_name
+
+    Type(units_scheme) :: scheme
 
     call initialise_units()
 
@@ -67,6 +71,28 @@ contains
 
     unit_test = convert_units(1.0_wp, 'k_B', 'm^2.kg.s^-2/K')
     Call assert(unit_test, kB_JK, "k_b->m^2.kg.s^-2/K fail")
+
+    call to_out_units(1.0_wp, 'Ang', unit_test, out_unit=unit_name)
+    Call assert(unit_test, 1.0_wp, "Out units internal fail")
+    Call assert(unit_name, 'internal_l', "Out units internal fail")
+
+    scheme = init_scheme( &
+         length = 'm', &
+         time = 's', &
+         mass = 'kg', &
+         charge = 'C', &
+         energy = 'J', &
+         pressure = 'Pa', &
+         force = 'N', &
+         velocity = 'm/s', &
+         power = 'W', &
+         surf_ten = 'N/m', &
+         emf = 'V')
+    call set_out_units(scheme)
+
+    call to_out_units(1.0_wp, 'Ang', unit_test, out_unit=unit_name)
+    Call assert(unit_test, 1.0e-10_wp, "Out units internal fail")
+    Call assert(unit_name, 'm', "Out units internal fail")
 
     call destroy_units()
 
