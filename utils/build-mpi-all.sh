764298f60d0476e7330e75316b61308800926040
alin m elena <alin@elena.space>
2022-04-23 15:51:05 +0100

    enforce plumed version

--- utils/build-mpi-all.sh
+++ utils/build-mpi-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-module load gnu/7 openmpi/3.1.6 netcdf-fortran/4.5.2  netcdf/4.7.3
+module load gnu/7 openmpi/3.1.6 
 module load plumed/gnu
 module load kim/gcc
 module load openblas/0.3.10
 
 mkdir build-mpi-all
 pushd build-mpi-all
-cmake ../ -DWITH_KIM=ON -DWITH_PLUMED=ON -DWITH_NETCDF=ON -DINTERNAL_KIM=off -DINTERNAL_PLUMED=off -DWITH_EVB=on
+cmake ../ -DWITH_KIM=ON -DWITH_PLUMED=ON -DPLUMED_VERSION=2.4.2 -DINTERNAL_KIM=off -DINTERNAL_PLUMED=off -DWITH_EVB=on
 make -j10
 
--- utils/build-mpi-plumed.sh
+++ utils/build-mpi-plumed.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-module load gnu/7 openmpi/3.1.6 netcdf-fortran/4.5.2  netcdf/4.7.3
+module load gnu/7 openmpi/3.1.6 
 module load plumed/gnu
 
 mkdir build-mpi-plumed
 pushd build-mpi-plumed
-cmake ../ -DWITH_PLUMED=ON -DINTERNAL_PLUMED=off 
+cmake ../ -DWITH_PLUMED=ON -DINTERNAL_PLUMED=off -DPLUMED_VERSION=2.4.2 
 make -j10
 
 
