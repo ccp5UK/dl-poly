ec1f994ebe0c65312b69b3874aba34f654b7e0b6
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2022-10-07 15:07:45 +0100

    Fix intel-testing-paranoid

--- utils/intel-testing-paranoid.sh
+++ utils/intel-testing-paranoid.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-module load intel/2020a
-module load gnu openblas
+module load intel/2022a
 mpr=`which mpirun`
+
 rm -rf build-intel-testing-p
 mkdir build-intel-testing-p
 pushd build-intel-testing-p
+
 FC=ifort cmake ../ -DBUILD_TESTING=ON  -DCMAKE_BUILD_TYPE=Debug -DMPI_Fortran_COMPILER=mpiifort -DMPIEXEC=$mpr -DMPIEXEC_PREFLAGS="-check-mpi" -DWITH_PLUMED=off -DWITH_EVB=ON && make -j10 && ctest --output-on-failure -j 1 -E TEST2[89]
