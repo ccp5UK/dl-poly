d3b8ea217b70bbdddd9b55c0a354f4c052f7a21b
alin m elena <alin@elena.space>
2023-02-10 09:26:12 +0000

    [ci] allow oversubscription

--- utils/mpi-testing-paranoid.sh
+++ utils/mpi-testing-paranoid.sh
@@ -5,4 +5,5 @@ module load foss/2022a Python
 
 folder="build-mpi-testing-paranoid"
 rm -rf $folder && mkdir $folder && pushd $folder
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DWITH_ASSERT=ON -DBUILD_TESTING=ON -DWITH_PLUMED=Off -DWITH_EVB=On && make -j10 && ctest --output-on-failure -E TEST2[89]  --timeout 300
+export OMPI_MCA_rmaps_base_oversubscribe=true
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DWITH_PLUMED=Off -DWITH_EVB=On && make -j10 && ctest --output-on-failure -E TEST2[89]  --timeout 300
