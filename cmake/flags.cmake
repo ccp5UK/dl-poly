3d5dd3d0da50df53b8936c382adfedfc929bf45b
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-11-16 14:01:52 +0000

    Bump standard up to Fortran 2018 and enforce everywhere

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -3,19 +3,19 @@ if (NOT FLAGS_SET)
     if(${CMAKE_Fortran_COMPILER_ID} STREQUAL "GNU")
 
       if (${CMAKE_Fortran_COMPILER_VERSION} VERSION_GREATER "7.9.9" )
-        set(CMAKE_Fortran_FLAGS_DEBUG "-g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2008 -pedantic -fbacktrace    -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42 -finit-derived  -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe " CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
-        set(CMAKE_Fortran_FLAGS_DEBUG2 "-static-libasan -g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2008 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42 -finit-derived -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe -fsanitize=undefined,leak,address" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
+        set(CMAKE_Fortran_FLAGS_DEBUG "-g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2018 -pedantic -fbacktrace    -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42 -finit-derived  -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe " CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
+        set(CMAKE_Fortran_FLAGS_DEBUG2 "-static-libasan -g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2018 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42 -finit-derived -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe -fsanitize=undefined,leak,address" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
       else()
-        set(CMAKE_Fortran_FLAGS_DEBUG "-g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2008 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42  -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
+        set(CMAKE_Fortran_FLAGS_DEBUG "-g -Wextra -Wuse-without-only -frecord-gcc-switches -O0 -std=f2018 -pedantic -fbacktrace -fcheck=all -finit-integer=2147483647 -finit-real=snan -finit-logical=true -finit-character=42  -ffpe-trap=invalid,zero,overflow -fdump-core -fstack-protector-all -Wall -pipe" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
       endif()
-      set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast -ftree-vectorize -funroll-loops -ffast-math" CACHE STRING "Flags used by the Fortran compiler during RELEASE builds." FORCE)
-      set(CMAKE_Fortran_FLAGS_PROFILE  "-Ofast -ftree-vectorize -funroll-loops -ffast-math -pg" CACHE STRING "Flags used by the Fortran compiler during PROFILE builds." FORCE)
+      set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast -std=f2018 -ftree-vectorize -funroll-loops -ffast-math" CACHE STRING "Flags used by the Fortran compiler during RELEASE builds." FORCE)
+      set(CMAKE_Fortran_FLAGS_PROFILE  "-Ofast -std=f2018  -ftree-vectorize -funroll-loops -ffast-math -pg" CACHE STRING "Flags used by the Fortran compiler during PROFILE builds." FORCE)
 
     elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Intel")
 
-      set(CMAKE_Fortran_FLAGS_DEBUG "-g -O0 -stand f08 -traceback -C -fp-stack-check -ftrapuv -init=snan -init=arrays" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
-      set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast" CACHE STRING "Flags used by the Fortran compiler during RELEASE builds." FORCE)
-      set(CMAKE_Fortran_FLAGS_PROFILE "-Ofast -pg -qopt-report=5" CACHE STRING "Flags used by the Fortran compiler during PROFILE builds." FORCE)
+      set(CMAKE_Fortran_FLAGS_DEBUG "-g -O0 -stand f18 -traceback -C -fp-stack-check -ftrapuv -init=snan -init=arrays" CACHE STRING "Flags used by the Fortran compiler during DEBUG builds." FORCE)
+      set(CMAKE_Fortran_FLAGS_RELEASE "-Ofast -stand f18" CACHE STRING "Flags used by the Fortran compiler during RELEASE builds." FORCE)
+      set(CMAKE_Fortran_FLAGS_PROFILE "-Ofast -stand f18 -pg -qopt-report=5" CACHE STRING "Flags used by the Fortran compiler during PROFILE builds." FORCE)
 
     elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL "Cray")
 
@@ -34,6 +34,6 @@ if (NOT FLAGS_SET)
   else()
     message(INFO "no flags preset")
   endif()
-  
+
   set(FLAGS_SET 1 CACHE INTERNAL "Flags are set")
 endif()
