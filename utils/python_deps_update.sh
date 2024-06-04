f808c087335c680f90dbf414e02496a434c61fce
Harvey Devereux <h.devereux@qmul.ac.uk>
2024-06-04 10:39:53 +0000

    Fix typo in python update script

--- utils/python_deps_update.sh
+++ utils/python_deps_update.sh
@@ -4,6 +4,6 @@
 module purge
 module load foss/2022a Python
 
-python3 -m pip uinstall dlpoly-py ruamel.yaml
+python3 -m pip uninstall -y dlpoly-py ruamel.yaml 
 python3 -m pip install -U git+https://gitlab.com/drFaustroll/dlpoly-py.git ruamel.yaml 
 
